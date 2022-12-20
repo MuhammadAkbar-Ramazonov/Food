70807af72d4c8bd9c4c18b7bbd81e03f11f43b80
AzamatXakimov <typetiger59@gmail.com>
2022-12-20 17:48:18 +0500

    add comments

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,10 @@ import {Route, Routes} from "react-router-dom";
 import { HotDishes } from "./HotDishes";
 export const Home = () => {
     return<>
+        {/* Bu yerda Home ni Head qismi ulanadi */}
         {/* </>  */}
+
+        Bu joyda ong tarafti Home page di Orders #34562
         <Routes>
             <Route index element={<HotDishes />}/>
             {/* <Route path="cold" element={</>}/> */}
