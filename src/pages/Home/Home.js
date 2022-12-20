ca2b5455e908ee947b0427aceae0b5833443f0ae
AzamatXakimov <typetiger59@gmail.com>
2022-12-20 17:36:50 +0500

    Fix

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,15 @@
-
+import {Route, Routes} from "react-router-dom";
+import { HotDishes } from "./HotDishes";
 export const Home = () => {
     return<>
-        
+        {/* </>  */}
+        <Routes>
+            <Route index element={<HotDishes />}/>
+            {/* <Route path="cold" element={</>}/> */}
+            {/* <Route path="soup" element={</>}/> */}
+            {/* <Route path="grill" element={</>}/> */}
+            {/* <Route path="appatizer" element={</>}/> */}
+            {/* <Route path="dessert" element={</>}/> */}
+        </Routes>
     </>
 }
