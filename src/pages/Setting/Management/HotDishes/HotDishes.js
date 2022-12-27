0ab0e764964be07b4f873ee6211e1d29bc099790
Muhammad Akbar <ramazonov3600@gmail.com>
2022-12-27 14:28:02 +0500

    fix bag

--- src/pages/Setting/Management/HotDishes/HotDishes.js
+++ src/pages/Setting/Management/HotDishes/HotDishes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { hotDishes } from "../../../../assets/data/Dishes";
 import { PlusBtn } from "../../../../assets/images/icons/icons";
+import { Modal } from "../../../../components/Modal/Modal";
 import "./HotDishes.scss";
 import { HotDishesItem } from "./HotDishesItem";
 export const HotDishes = () => {
@@ -8,20 +9,29 @@ export const HotDishes = () => {
 		<div className='settings-hot-dishes-inner'>
 			<ul className='settings-hot-dishes-inner-list'>
 				<li className='settings-hot-dishes-inner-item-edit'>
-					<PlusBtn />
-					<p className='settings-hot-dishes-inner-item-edit-title'>Add new dish</p>
+					<button
+						type='button'
+						class='settings-hot-dishes-inner-item-edit-btn'
+						data-bs-toggle='modal'
+						data-bs-target='#exampleModal'
+					>
+						<PlusBtn />
+						<p className='settings-hot-dishes-inner-item-edit-title'>
+							Add new dish
+						</p>
+					</button>
 				</li>
 				{hotDishes.map((item, index) => (
 					<HotDishesItem key={index} obj={item} />
 				))}
 			</ul>
-
 			<div className='settings-hot-dishes-inner-btn-wrap'>
 				<button className='settings-hot-dishes-inner-discard'>
 					Discard Changes
 				</button>
 				<button className='settings-hot-dishes-inner-save'>Save Changes</button>
 			</div>
+			<Modal />
 		</div>
 	);
 };
